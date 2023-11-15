6d0585fd60ae0b2a95b095a3a920f916fc81c8b0
Julien Ripouteau <julien@ripouteau.com>
2023-11-15 03:52:23 +0100

    feat: add v1 configure step

--- configure.ts
+++ configure.ts
@@ -9,7 +9,107 @@
 
 import type Configure from '@adonisjs/core/commands/configure'
 
+const ADAPTERS = ['Vue 3', 'React', 'Svelte'] as const
+const ADAPTERS_INFO: {
+  [K in (typeof ADAPTERS)[number]]: {
+    dependencies: {
+      name: string
+      isDevDependency: boolean
+    }[]
+    ssrDependencies?: {
+      name: string
+      isDevDependency: boolean
+    }[]
+  }
+} = {
+  'Vue 3': {
+    dependencies: [
+      { name: '@inertiajs/vue3', isDevDependency: false },
+      { name: 'vue', isDevDependency: false },
+      { name: '@vitejs/plugin-vue', isDevDependency: true },
+    ],
+    ssrDependencies: [{ name: '@vue/server-renderer', isDevDependency: false }],
+  },
+  'React': {
+    dependencies: [
+      { name: '@inertiajs/inertia-react', isDevDependency: false },
+      { name: 'react', isDevDependency: false },
+      { name: 'react-dom', isDevDependency: false },
+      { name: '@vitejs/plugin-react', isDevDependency: true },
+      { name: '@types/react', isDevDependency: true },
+      { name: '@types/react-dom', isDevDependency: true },
+    ],
+  },
+  'Svelte': {
+    dependencies: [
+      { name: '@inertiajs/inertia-svelte', isDevDependency: false },
+      { name: 'svelte', isDevDependency: false },
+      { name: '@sveltejs/vite-plugin-svelte', isDevDependency: true },
+    ],
+  },
+}
+
 /**
  * Configures the package
  */
-export async function configure(command: Configure) {}
+export async function configure(command: Configure) {
+  /**
+   * Prompt for adapter
+   */
+  const adapter = await command.prompt.choice(
+    'Select the Inertia adapter you want to use',
+    ADAPTERS,
+    { name: 'adapter' }
+  )
+  const pkgToInstall = ADAPTERS_INFO[adapter].dependencies
+
+  /**
+   * Prompt for SSR
+   */
+  const withSsr = await command.prompt.confirm('Do you want to enable server-side rendering?', {
+    name: 'ssr',
+  })
+
+  if (withSsr) {
+    pkgToInstall.push(...(ADAPTERS_INFO[adapter].ssrDependencies || []))
+  }
+
+  const codemods = await command.createCodemods()
+
+  /**
+   * Publish provider
+   */
+  await codemods.updateRcFile((rcFile) => {
+    rcFile.addProvider('@adonisjs/inertia/inertia_provider')
+  })
+
+  /**
+   * Add Inertia middleware
+   */
+  codemods.registerMiddleware('router', [
+    { path: '@adonisjs/inertia/inertia_middleware', position: 'after' },
+  ])
+
+  /**
+   * Publish config
+   */
+  await command.publishStub('config.stub')
+
+  /**
+   * Install packages
+   */
+  const shouldInstallPackages = await command.prompt.confirm(
+    `Do you want to install dependencies ${pkgToInstall.map((pkg) => pkg.name).join(', ')}?`,
+    { name: 'install' }
+  )
+
+  if (shouldInstallPackages) {
+    command.installPackages(pkgToInstall)
+  } else {
+    command.listPackagesToInstall(pkgToInstall)
+  }
+
+  command.logger.success(
+    'Inertia was configured successfully. Please note that you still need to update your vite config, setup your Edge root view and others things. Read the docs for more info.'
+  )
+}
---
+++ tests/configure.spec.ts
@@ -0,0 +1,81 @@
+/*
+ * @adonisjs/inertia
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { test } from '@japa/runner'
+import { fileURLToPath } from 'node:url'
+import { IgnitorFactory } from '@adonisjs/core/factories'
+import Configure from '@adonisjs/core/commands/configure'
+
+const BASE_URL = new URL('./tmp/', import.meta.url)
+
+async function setupApp() {
+  const ignitor = new IgnitorFactory()
+    .withCoreProviders()
+    .withCoreConfig()
+    .create(BASE_URL, {
+      importer: (filePath) => {
+        if (filePath.startsWith('./') || filePath.startsWith('../')) {
+          return import(new URL(filePath, BASE_URL).href)
+        }
+
+        return import(filePath)
+      },
+    })
+
+  const app = ignitor.createApp('web')
+  await app.init().then(() => app.boot())
+
+  const ace = await app.container.make('ace')
+  ace.ui.switchMode('raw')
+
+  return { ace, app }
+}
+
+test.group('Configure', (group) => {
+  group.tap((t) => t.timeout(10_000))
+
+  group.each.setup(async ({ context }) => {
+    context.fs.baseUrl = BASE_URL
+    context.fs.basePath = fileURLToPath(BASE_URL)
+
+    await context.fs.create('.env', '')
+    await context.fs.createJson('tsconfig.json', {})
+    await context.fs.create('adonisrc.ts', `export default defineConfig({})`)
+    await context.fs.create(
+      'start/kernel.ts',
+      `
+      import router from '@adonisjs/core/services/router'
+      import server from '@adonisjs/core/services/server'
+
+      router.use([
+        () => import('@adonisjs/core/bodyparser_middleware'),
+        () => import('@adonisjs/session/session_middleware'),
+        () => import('@adonisjs/shield/shield_middleware'),
+        () => import('@adonisjs/auth/initialize_auth_middleware'),
+      ])
+    `
+    )
+  })
+
+  test('add provider, config file, and middleware', async ({ assert }) => {
+    const { ace } = await setupApp()
+
+    ace.prompt.trap('adapter').replyWith('Vue 3')
+    ace.prompt.trap('ssr').reject()
+    ace.prompt.trap('install').reject()
+
+    const command = await ace.create(Configure, ['../../index.js'])
+    await command.exec()
+
+    await assert.fileExists('config/inertia.ts')
+    await assert.fileExists('adonisrc.ts')
+    await assert.fileContains('adonisrc.ts', '@adonisjs/inertia/inertia_provider')
+    await assert.fileContains('start/kernel.ts', '@adonisjs/inertia/inertia_middleware')
+  })
+})
