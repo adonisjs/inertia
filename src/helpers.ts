1615c2f44e7e15bf2c9d319110f7905b66b11668
Julien Ripouteau <julien@ripouteau.com>
2024-02-27 01:16:22 +0100

    feat: resolvePageComponent helper

---
+++ src/helpers.ts
@@ -0,0 +1,25 @@
+/**
+ * Utility function to resolve a page component
+ *
+ * @example
+ *    return resolvePageComponent(
+ *      `./pages/${name}.vue`,
+ *       import.meta.glob<DefineComponent>("./pages/**\/*.vue"
+ *    )
+ */
+export async function resolvePageComponent<T>(
+  path: string | string[],
+  pages: Record<string, Promise<T> | (() => Promise<T>)>
+): Promise<T> {
+  for (const p of Array.isArray(path) ? path : [path]) {
+    const page = pages[p]
+
+    if (typeof page === 'undefined') {
+      continue
+    }
+
+    return typeof page === 'function' ? page() : page
+  }
+
+  throw new Error(`Page not found: ${path}`)
+}
