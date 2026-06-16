39dcdf22f6ffc934b36e8e40fcbf998a8ef9703f
Florian Zdrada <56303005+flozdra@users.noreply.github.com>
2026-06-16 12:25:08 +0200

    feat: allow returning null prop (#89)

--- src/props.ts
+++ src/props.ts
@@ -329,6 +329,10 @@ async function unpackPropValue(
   value: UnPackedPageProps<JSONDataTypes>,
   containerResolver: ContainerResolver<any>
 ) {
+  // Allow returning null values without serialization
+  if (value === null) {
+    return null
+  }
   return inertiaSerializer.serialize(value, containerResolver) as Promise<JSONDataTypes>
 }
 
